af81bbc0b181f58e6893b161b2096f422b2f14a6
ortolo01 <78178039+gabrieleOrtolani@users.noreply.github.com>
2022-04-04 23:10:27 +0200

    Update thread_strjob_tls.c

--- thread_strjob_tls.c
+++ thread_strjob_tls.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+
 
 #define KRED  "\x1B[31m"
 #define KGRN  "\x1B[32m"
@@ -17,14 +20,14 @@ void *thread1(void * x){
    int i=0;
    char temp;
    printf("%sI'm the thread %d\n\tinitial thread1 buffer %s; len buffer %ld\n%s",KRED,gettid(),buffer,strlen(buffer),KWHT);
-for (i = 0; i < len/2; i++)  
-    {  
-        // temp variable use to temporary hold the string  
-        temp = buffer[i];  
-        buffer[i] = buffer[len - i - 1];  
-        buffer[len - i - 1] = temp;  
+for (i = 0; i < len/2; i++)
+    {
+        // temp variable use to temporary hold the string
+        temp = buffer[i];
+        buffer[i] = buffer[len - i - 1];
+        buffer[len - i - 1] = temp;
         printf("%sinvert the characters at the edge of the string:%s\n%s",KBLU,buffer,KWHT);
-    }  
+    }
    printf("%s-----------------------------------------------------------------%s\n",KGRN,KWHT);
    pthread_exit((void*)status);
 }
@@ -49,12 +52,12 @@ int main(int argc, char **argv){
    void *status;
    void *status1;
    pthread_t t1;
-   pthread_create(&t1,NULL,thread1,NULL);  
+   pthread_create(&t1,NULL,thread1,NULL);
    pthread_join(t1,&status);
    pthread_t t2;
-   pthread_create(&t2,NULL,thread2,NULL);  
-   pthread_join(t2,&status1);   
+   pthread_create(&t2,NULL,thread2,NULL);
+   pthread_join(t2,&status1);
    printf("%sI'm the main %d\n\tend main buffer %s; len buffer %ld\n%s",KRED,getpid(),buffer,strlen(buffer),KWHT);
-   printf("%s\n",buffer);
 }
+
 
